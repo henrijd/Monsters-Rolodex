debe31a215ce5aea49f3070b70884a571bb0cfe8
hyp3r <henrijdom@gmail.com>
2022-09-05 20:27:52 +0200

    More CSS for the Card Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import './card-list.styles.css'
+
 class CardList extends Component {
 
     render() {
@@ -7,9 +9,17 @@ class CardList extends Component {
         
         return (
             <div className='card-list'>
-                {monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+                    
+                    return (
+                    <div className='card-container' key={id}>
+                        <img alt= {`monster ${name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                    );
+                })}
             </div>
         )
     }
